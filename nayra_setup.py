d4ff9890e7152f95d8d7a0bdba6a01df5d4e60b8
Jose Laruta <eduardo.laruta@gmail.com>
2019-02-16 04:13:59 -0400

    initial actions insert

--- nayra_setup.py
+++ nayra_setup.py
@@ -1,5 +1,14 @@
 from app import app, db
-from app.models import Word
+from app.models import Word, Action
+
+
+actions_list = [
+    ['traction', 'adelante'],
+    ['led', 'blink'],
+    ['traction', ' atras'],
+    ['greet', 'hola'],
+    ['greet', 'adios']
+]
 
 with open("es-ES/pronounciation-dictionary.dict", "r") as f:
     content = f.read().split("\n")
@@ -11,6 +20,11 @@ for word in words:
     w = Word(word=word)
     db.session.add(w)
     i += 1
+
+for action in actions_list:
+    a = Action(category=action[0], action=action[1])
+    db.session.add(a)
+
 
 
 db.session.commit()
